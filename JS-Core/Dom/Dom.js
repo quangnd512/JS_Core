44c16c1088a043931a8a6484bc16d3e8b647f08a
quangnd512 <quangnd512@gmail.con>
2020-05-22 18:24:16 +0700

    update code 22/5

---
+++ JS-Core/Dom/Dom.js
@@ -0,0 +1,18 @@
+// var domValue = document.getElementById("txt");
+// document.write(domValue.name);
+
+// var domValue = document.getElementsByTagName("input");
+// document.write(domValue[0].id);
+
+// var domValue = document.getElementsByClassName("url_class_frm");
+// document.write(domValue[0].id);
+
+// document.getElementById("hello").innerHTML= "Hello World";
+
+// document.getElementById("hello").style = "color: red;";
+
+function ChangeColor(){
+    document.getElementById("hello").style.color = "red";
+}
+
+
---
+++ JS-Core/Mang/Array.js
@@ -0,0 +1,12 @@
+var test = [
+    'test1',
+    'test2',
+    1,
+    null,
+    function test3() {
+        return 2;
+    }
+];
+console.log(test);
+console.log(test[1]);
+
