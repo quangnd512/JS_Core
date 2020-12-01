d183a43a202c9fb4ef8526d53f5b6ab1685aaefe
quangnd512 <quangnd512@gmail.con>
2020-12-01 09:28:37 +0700

    Add document Jquery

---
+++ JS-Core/Bai-tap/Bai3.js
@@ -0,0 +1,3 @@
+// Hien thi ngay thang 
+var date = new Date();
+document.write("Bay gio la: "+date.getHours()+" giờ "+date.getMinutes()+" phút "+" ngày "+date.getDate()+" tháng "+date.getMonth()+" nam "+date.getFullYear());
