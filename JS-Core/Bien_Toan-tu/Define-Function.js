942317170c355f18fff6d5cfb209283979c95b7b
QuangND <quangnd512@gmail.com>
2023-01-16 17:41:50 +0700

    add document

--- JS-Core/Bien_Toan-tu/Define-Function.js
+++ JS-Core/Bien_Toan-tu/Define-Function.js
@@ -11,5 +11,36 @@
 // alert(a);
 // alert(eval(a));
 
-var a = prompt("Nhap bieu thuc can tinh: ");
-document.write(eval(a));
+// var a = prompt("Nhap bieu thuc can tinh: ");
+// document.write(eval(a));
+
+var keyEmail = 'email';
+// Object
+var myInfo = {
+    name: 'Quang ND',
+    age: '27',
+    address: 'Ha Noi',
+    [keyEmail]: 'quangnd512@gmail.com',
+    
+    // Khai báo function trong object
+    getName: function() {
+        return this.name;
+    }
+}
+
+// Gọi đối tượng function
+myInfo.getName() // object myInfo
+
+// Đổi 1 giá trị trong object
+myInfo.name = 'Quang ND 123'; //giá trị name trong object = Quang ND 123
+
+// Sửa key
+myInfo['age-name'] = '27';
+
+// Lấy value
+myInfo.name;
+myInfo['name'];
+
+// xóa Key
+delete myInfo.age
+
